208d0c6824c04237e7d6f144dfd2ea99fc091cd4
sheel-d <1103314650@qq.com>
2016-06-29 09:25:39 +0800

    react

---
+++ React/HTML/6.js
@@ -0,0 +1,10 @@
+/**
+ * Created by Administrator on 2016/5/18.
+ */
+window.onload = function(){
+    ReactDOM.render(
+        <h1>Hello Word!</h1>,
+        document.getElementById('demo')
+    );
+};
+
